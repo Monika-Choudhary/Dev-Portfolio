2b8ac414fd3886840d02f119ba666d2e57a5125e
Monika-Choudhary <monikachahota@gmail.com>
2025-03-20 11:19:59 +0100

    Refactor NavBar component to enhance active link styling with conditional classes

--- src/navigation/NavBar.tsx
+++ src/navigation/NavBar.tsx
@@ -3,16 +3,40 @@ import { NavLink } from "react-router";
 function NavBar() {
   return (
     <nav className="flex justify-center space-x-30">
-      <NavLink to="/" end>
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive ? "bg-fuchsia-900 p-2" : "text-white"
+        }
+        end
+      >
         Home
       </NavLink>
-      <NavLink to="/about-me" end>
+      <NavLink
+        to="/about-me"
+        className={({ isActive }) =>
+          isActive ? "bg-fuchsia-900 p-2" : "text-white"
+        }
+        end
+      >
         About Me
       </NavLink>
-      <NavLink to="/my-projects" end>
+      <NavLink
+        to="/my-projects"
+        className={({ isActive }) =>
+          isActive ? "bg-fuchsia-900 p-2" : "text-white"
+        }
+        end
+      >
         My Projects
       </NavLink>
-      <NavLink to="/contact" end>
+      <NavLink
+        to="/contact"
+        className={({ isActive }) =>
+          isActive ? "bg-fuchsia-900 p-2" : "text-white"
+        }
+        end
+      >
         Contact
       </NavLink>
     </nav>
