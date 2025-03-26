c35b4cc010ea48710d97202bb4b61651ca66b704
Monika-Choudhary <monikachahota@gmail.com>
2025-03-26 12:49:28 +0100

    Add LinkedIn icons to SocialMediaLinks component; define icon size in CSS

--- src/components/SocialMediaLinks.tsx
+++ src/components/SocialMediaLinks.tsx
@@ -1,14 +1,18 @@
 import { FaGithub, FaInstagram } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa6";
 
 function SocialMediaLinks() {
   return (
     <div className="absolute top-1/2 right-5 transform -translate-y-1/2 flex flex-col items-end space-y-5">
-      <a href="https://github.com/Monika-Choudhary" target="_blank">
-        {" "}
-        <FaGithub className="w-9 h-9" />
+      <a href="" target="blank">
+        <FaLinkedinIn className="icon-size" />
       </a>
-      <FaInstagram className="w-9 h-9 " />
-    
+      <a href="https://github.com/Monika-Choudhary" target="_blank">
+        <FaGithub className="icon-size" />
+      </a>
+      <a href="" target="blank">
+        <FaInstagram className="icon-size" />
+      </a>
     </div>
   );
 }
