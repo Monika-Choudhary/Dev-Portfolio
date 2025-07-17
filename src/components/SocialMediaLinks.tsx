b448794f51b6b1e781e106a35cb5189399e9559f
Monika-Choudhary <monikachahota@gmail.com>
2025-07-17 12:53:46 +0200

    Refactor SocialMediaLinks component: remove unused Instagram icon and update LinkedIn link and email link

--- src/components/SocialMediaLinks.tsx
+++ src/components/SocialMediaLinks.tsx
@@ -1,17 +1,17 @@
-import { FaGithub, FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa6";
 
 function SocialMediaLinks() {
   return (
     <div className="absolute top-1/2 right-5 transform -translate-y-1/2 flex flex-col items-end space-y-5">
-      <a href="" target="blank">
+      <a href="https://www.linkedin.com/in/monikakaurchoudhary" target="blank">
         <FaLinkedinIn className="icon-size" />
       </a>
       <a href="https://github.com/Monika-Choudhary" target="_blank">
         <FaGithub className="icon-size" />
       </a>
-      <a href="" target="blank">
-        <FaInstagram className="icon-size" />
+      <a href="mailto:monikakaurchoudhary@gmail.com" className="rotate-90">
+        monikakaurchoudhary@gmail.com
       </a>
     </div>
   );
